2ad7b3a86ffdbe7ddd4cce87dea00100301ce851
surinder singh <surinder83singh@gmail.com>
2021-01-20 10:06:12 +0530

    cleanup

--- types/custom-types.d.ts
+++ types/custom-types.d.ts
@@ -8,24 +8,3 @@ export interface SubscriberItem{
 }
 
 export declare type SubscriberItemMap = Map<string, SubscriberItem[]>;
-
-export declare class Client{
-	constructor(options: any);
-	onConnect(callback: Function): void;
-    onConnectFailure(callback: Function): void;
-    onError(callback: Function): void;
-    onDisconnect(callback: Function): void;
-    disconnect(): void;
-    clearPending(): void;
-    close(): void;
-    createStream(): any;
-    initIntake(stream: IStream): void;
-    handleIntake(o: IData): void;
-    setIntakeHandler(fn: Function): void;
-    post(name: string, args?: any): boolean;
-    call(method: string, data: any): Promise<unknown>;
-    subscribe<T>(subject: string, data: any, callback: Function): RPC.SubPromise<T>;
-    subject2EventName(subject: string): string;
-    unSubscribe(subject: string, uid?: string): void;
-    connect(): void;
-}
