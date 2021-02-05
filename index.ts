549f1dfaec255d4dcbbf8a2337c00cdb2209118d
surinder singh <surinder83singh@gmail.com>
2021-02-06 04:04:50 +0530

    Storage export issue

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-import {log, Wallet as Core} from "kaspa-wallet";
+import {log, Wallet as Core, Storage} from "kaspa-wallet";
 import {WalletWrapper as Wallet, initKaspaFramework, workerLog} from "./lib/wallet-wrapper";
-export {Wallet, initKaspaFramework, log, workerLog, Core}
+export {Wallet, initKaspaFramework, log, workerLog, Core, Storage}
